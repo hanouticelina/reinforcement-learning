3fdb3ba6a4d4f0e80c676f6baf838acdc39b5d69
Celina Hanouti <hanouticelina@outlook.com>
2021-02-24 23:18:15 +0100

    Update TME 3. Q-Learning.py

--- TME 3. Q-Learning/TME 3. Q-Learning.py
+++ TME 3. Q-Learning/TME 3. Q-Learning.py
@@ -90,6 +90,46 @@ class Sarsa(object):
         self.lastobs = self.obs
         self.lasta = action
 
+        
+class Dyna_Q(object):
+    def __init__(self, action_space, explorer, gamma, alpha, alpha_R, alpha_P, k):
+        self.action_space = action_space
+        self.explorer = explorer
+        self.Q = defaultdict(lambda : np.zeros(4))
+        self.old_at = None
+        self.old_st= None
+        self.gamma = gamma
+        self.alpha = alpha
+        self.alpha_P = alpha_P
+        self.alpha_R = alpha_R
+        self.k = k
+        self.P = defaultdict(lambda : defaultdict(lambda : np.zeros(4)))
+        self.R = defaultdict(lambda : defaultdict(lambda : np.zeros(4)))
+
+
+    # after execution of a_(t-1) we go to st and we get reward = r_(t-1)
+    def act(self, s_t, reward, done):
+        s_t = str(s_t.tolist())
+        a_t = self.explorer.choose(s_t = s_t,Q = self.Q)
+        self.learn(self.old_st, self.old_at, reward, s_t)
+        self.old_at = a_t
+        self.old_st = s_t
+        return a_t
+
+    def learn(self, old_st, old_at, reward, s_t):
+        max = self.Q[s_t].max()
+        # value-based part
+        self.Q[old_st][old_at] = self.Q[old_st][old_at] + self.alpha * (reward + self.gamma * max  - self.Q[old_st][old_at])
+        # model-based part
+        self.R[old_st][s_t][old_at] = self.R[old_st][s_t][old_at] + self.alpha_R * (reward - self.R[old_st][s_t][old_at])
+        self.P[s_t][old_st][old_at] =  self.P[s_t][old_st][old_at] + self.alpha_R * (int((s_t != old_st)) - self.P[s_t][old_st][old_at])
+
+        states = np.random.choice(np.array(list(self.P.keys())) , size = self.k )
+        actions = np.array( [self.action_space.sample() for i in range(self.k) ] )
+        for state, action in zip(states, actions):
+            d1 = self.Q[state][action]
+            d2 = sum(self.P[FutureState][state][action] * (self.R[state][FutureState][action] + self.gamma * self.Q[FutureState].max()) for FutureState in self.P)
+            self.Q[state][action] =  d1+ self.alpha *( d2 - d1)       
 
 if __name__ == '__main__':
 
